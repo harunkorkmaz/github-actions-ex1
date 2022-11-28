e867a13a5253333edefded961a483e6b344bf2d4
Harun <1harunkorkmaz@gmail.com>
2022-11-28 16:13:55 +0300

    try using export, and lets see does it work

--- test/test_App.py
+++ test/test_App.py
@@ -1,10 +1,12 @@
-import os
-import sys
+# import os
+# import sys
 
-currentPosition = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(currentPosition, '../'))
+# currentPosition = os.path.dirname(os.path.realpath(__file__))
+# sys.path.append(os.path.join(currentPosition, '../'))
 
-from src.app import index
+# from src.app import index
+
+from app import index
 
 def test():
     assert index() == 'Hello World'
